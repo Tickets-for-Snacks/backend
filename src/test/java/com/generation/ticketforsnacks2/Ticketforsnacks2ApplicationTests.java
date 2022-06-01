72b4fca0aeba96ddbf5c4df08a6ae37bea4e9475
Augusto Cesar <acsilvaluis@outlook.com>
2022-06-01 14:21:06 -0300

    Adição de Classe e atualização de Tabelas

---
+++ src/test/java/com/generation/ticketforsnacks2/Ticketforsnacks2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.ticketforsnacks2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ticketforsnacks2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
